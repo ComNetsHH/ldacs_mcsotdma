26515c3c7507466940d14c5855673862f9675915
Sebastian Lindner <sebastian.lindner@tuhh.de>
2021-09-08 15:36:36 +0200

    implemented new 'naive_random_access' contention method which just selects 1 random slot out of the next 100 idle ones; closes #82

--- BCLinkManager.cpp
+++ BCLinkManager.cpp
@@ -234,6 +234,12 @@ unsigned int BCLinkManager::getNumCandidateSlots(double target_collision_prob) c
 		unsigned int m = contention_estimator.getNumActiveNeighbors();
 		k = std::ceil(1.0 / (1.0 - std::pow(1.0 - target_collision_prob, 1.0 / m)));
 		coutd << "contention method: assume all " << m << " active neighbors are active again -> ";
+	// Don't make use of contention estimation in any way. Just select something out of the next 100 idle slots.
+	} else if (contention_method == ContentionMethod::naive_random_access) {
+		k = 100;
+		coutd << "contention method: naive random access -> ";
+	} else {
+		throw std::invalid_argument("BCLinkManager::getNumCandidateSlots for unknown contention method: '" + std::to_string(contention_method) + "'.");
 	}
 	unsigned int final_candidates = std::max(MIN_CANDIDATES, k);
 	coutd << "num_candidates=" << final_candidates << " -> ";
